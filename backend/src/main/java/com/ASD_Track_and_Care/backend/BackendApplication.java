082de92100a7dff83530cbf05d1ddc6ce5634a97
Susam Pudasaini <pudasainisusam@gmail.com>
2025-12-12 12:42:22 +0545

    Initial commit

---
+++ backend/src/main/java/com/ASD_Track_and_Care/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.ASD_Track_and_Care.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ASD_Track_and_Care/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ASD_Track_and_Care.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
